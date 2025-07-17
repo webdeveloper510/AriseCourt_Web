231c2df9b216ac58ad31ed36d2320e46eb61e3a7
webdeveloper510 <developerweb6@gmail.com>
2025-07-17 18:17:39 +0530

    reporting table/amount pv

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -353,7 +353,9 @@ const Dashboard = () => {
                     return (
                       <CTableRow key={i}>
                         {/* <CTableDataCell>#123</CTableDataCell> */}
-                        <CTableDataCell>{(currentPage - 1) * itemsPerPage + i + 1}</CTableDataCell>
+                        <CTableDataCell>
+                          {(currentPage - 1) * itemsPerPage + i + 1}
+                        </CTableDataCell>
                         <CTableDataCell title={item?.first_name}>
                           {item?.first_name?.length > 10
                             ? `${item?.first_name?.slice(0, 10)}...`
@@ -432,10 +434,17 @@ const Dashboard = () => {
                                   }}
                                   className="action_icons"
                                 >
-                                  <CIcon
-                                    icon={cilDelete}
-                                    className="delete_icon"
-                                  />{" "}
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    className="bi bi-trash-fill delete_icon"
+                                    viewBox="0 0 16 16"
+                                  >
+                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                                  </svg>
+                                 
                                   Delete
                                 </div>
                               </div>
