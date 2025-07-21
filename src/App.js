72469d77289fb1971a7b600c224213c5d195b6c5
webdeveloper510 <developerweb6@gmail.com>
2025-07-21 10:13:41 +0530

    navigate issue pv

--- src/App.js
+++ src/App.js
@@ -1,52 +1,57 @@
 // App.js
-import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { useColorModes } from '@coreui/react';
-import './scss/style.scss';
-import './scss/examples.scss';
-import ForgotEmail from './views/pages/forgotpassword/forgotemail';
-import NewPassword from './views/pages/forgotpassword/new-password';
-import { PrivateRoute, PublicRoute } from './utils/RouteGuards';
-import VerifyEmail from './views/admin/verifyEmail';
-import { getProfile } from './utils/api';
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useColorModes } from "@coreui/react";
+import "./scss/style.scss";
+import { useNavigate } from "react-router-dom";
+import "./scss/examples.scss";
+import ForgotEmail from "./views/pages/forgotpassword/forgotemail";
+import NewPassword from "./views/pages/forgotpassword/new-password";
+import { PrivateRoute, PublicRoute } from "./utils/RouteGuards";
+import VerifyEmail from "./views/admin/verifyEmail";
+import { getProfile } from "./utils/api";
 
-const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
-const Login = React.lazy(() => import('./views/pages/login/Login'));
-const Register = React.lazy(() => import('./views/pages/register/Register'));
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
+const DefaultLayout = React.lazy(() => import("./layout/DefaultLayout"));
+const Login = React.lazy(() => import("./views/pages/login/Login"));
+const Register = React.lazy(() => import("./views/pages/register/Register"));
+const Page404 = React.lazy(() => import("./views/pages/page404/Page404"));
+const Page500 = React.lazy(() => import("./views/pages/page500/Page500"));
 
 const App = () => {
-  const { setColorMode } = useColorModes('coreui-free-react-admin-template-theme');
+  const { setColorMode } = useColorModes(
+    "coreui-free-react-admin-template-theme"
+  );
   const storedTheme = useSelector((state) => state.theme);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    setColorMode('white');
+    setColorMode("white");
   }, []);
 
-   useEffect(() => {
-      getProfileData();
-    }, []);
-  
-    const getProfileData = () => {
-      getProfile()
-        .then((res) => {
-          if (res?.data.code == "200") {          
-          } else if (res?.data?.code == "token_not_valid") {            
-            navigate("/login");
-            localStorage.removeItem("user_access_valid_token");
-            localStorage.removeItem("logged_user_data");
-          }
-        })
-        .catch((error) => {
+  useEffect(() => {
+    getProfileData();
+  }, []);
+
+  const getProfileData = () => {
+    getProfile()
+      .then((res) => {
+        console.log("token_not_valid", res);
+        if (res?.data.code == "200") {
+        } else if (res?.data?.code == "token_not_valid") {
           navigate("/login");
-          console.log(error);
-        });
-    };
+          localStorage.removeItem("user_access_valid_token");
+          localStorage.removeItem("logged_user_data");
+        }
+      })
+      .catch((error) => {
+        navigate("/login");
+        console.log(error);
+      });
+  };
 
   return (
-    <BrowserRouter>
+  
       <Routes>
         {/* Public Auth Routes */}
         <Route
@@ -85,7 +90,7 @@ const App = () => {
           path="/verify-email/:id"
           element={
             // <PublicRoute>
-              <VerifyEmail />
+            <VerifyEmail />
             // </PublicRoute>
           }
         />
@@ -104,7 +109,7 @@ const App = () => {
           }
         />
       </Routes>
-    </BrowserRouter>
+   
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom' // <-- import this
 import 'core-js'
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify'
 
 import App from './App'
 import store from './store'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-    <ToastContainer/>
-  </Provider>,
+    <BrowserRouter> {/* <-- wrap App in this */}
+      <App />
+      <ToastContainer />
+    </BrowserRouter>
+  </Provider>
 )
