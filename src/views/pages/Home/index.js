80eb6fa10d976e823b6f2d3f9845c98542802654
webdeveloper510 <developerweb6@gmail.com>
2025-09-02 11:22:18 +0530

    add footer on home page pv

--- src/views/pages/Home/index.js
+++ src/views/pages/Home/index.js
@@ -67,30 +67,39 @@ const HomePage = () => {
               />
             </div>
           </div>
-          <div className="row mt-5 px-0 ps-0 pe-0 app_store_section" style={{ background: "#D2E3FF", borderRadius:"15px" }}>
-            <div className="col-md-6 text-center mx-0 px-0 ps-0 pe-0 ">
+          <div
+            className="row mt-5 px-0 ps-0 pe-0 app_store_section"
+            style={{borderRadius: "15px" }}
+          >
+            <div className="col-md-6 text-center mx-0 px-0 ps-0 pe-0 order_section1">
               <img
                 src={AppSectionImage}
                 alt="BannerImage"
-                style={{ width: "100%" }}              
+                style={{ width: "90%" }}
               />
             </div>
-            <div className="col-md-6 d-flex align-items-center px-5 py-4">
+            <div className="col-md-6 d-flex align-items-center px-5 py-4 order_section2">
               <div>
-                <img src={LoginLogo} alt="BannerImage" className="mb-5" />
-                <div className="row">
+                <img src={LoginLogo} alt="BannerImage" className="mb-3" />
+                <h2 className="download_app mb-3">Download Our App</h2>
+                <p className="lorem_text mb-3">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua.{" "}
+                </p>
+                <div className="row px-0">
                   <div className="col-md-6 my-2">
                     <img
                       src={AppStore}
                       alt="BannerImage"
-                      style={{ width: "100%" }}
+                      style={{ width: "100%", objectFit:"cover" }}
                     />
                   </div>
                   <div className="col-md-6 my-2">
                     <img
                       src={PlayStore}
                       alt="BannerImage"
-                      style={{ width: "100%" }}
+                      style={{ width: "100%" , objectFit:"cover"}}
                     />
                   </div>
                 </div>
